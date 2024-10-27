ae11935dde57f8c7814c368076359cdc40a679d8
runkyzhang <runkyzhang@gmail.com>
2024-10-27 12:14:20 +0800

    add comments

--- scaffolding/vf-user-base/user-base-interfaces/src/main/java/com/vf/user/base/interfaces/UserBaseApplication.java
+++ scaffolding/vf-user-base/user-base-interfaces/src/main/java/com/vf/user/base/interfaces/UserBaseApplication.java
@@ -1,16 +1,16 @@
 package com.vf.user.base.interfaces;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+// 启动（vm options）参数加入-Dspring.cloud.bootstrap.enabled=true。使用bootstrap.yml为启动配置。nacos配置中心需要这种方式启动
 @EnableDubbo
 @SpringBootApplication
-@ComponentScan(basePackages={"com.vf.user.base", "com.vf.common.architecture"})
+@ComponentScan(basePackages = {"com.vf.user.base", "com.vf.common.architecture"})
 @MapperScan(basePackages = "com.vf.user.base.infrastructure.mapper")
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class UserBaseApplication {
