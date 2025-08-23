7a91ba76fe5bad1abd8bd559158e07fee3d8df19
zhangrenjie <00545579@want-want.com>
2025-08-23 17:49:33 +0800

    feat(chat-client): 集成 MCP 客户端并优化 ChatClient 配置
    - 新增 WebFluxSseClientTransport配置，支持 MCP 客户端
    - 更新 ChatClient 配置，集成 ToolCallbackProvider
    - 在 application.yml 中添加 MCP 客户端相关配置
    -调整 pom.xml，注释掉不必要的依赖

--- demo/spring-ai-mcp-demo/chat-client/src/main/java/com/rz/web/demo/chat/config/AppConfig.java
+++ demo/spring-ai-mcp-demo/chat-client/src/main/java/com/rz/web/demo/chat/config/AppConfig.java
@@ -1,12 +1,25 @@
 package com.rz.web.demo.chat.config;
 
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Request;
+import io.modelcontextprotocol.client.transport.WebFluxSseClientTransport;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.mcp.client.autoconfigure.NamedClientMcpTransport;
+import org.springframework.ai.mcp.client.autoconfigure.properties.McpSseClientProperties;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@EnableConfigurationProperties({McpSseClientProperties.class})
 @Configuration
 public class AppConfig {
     @Bean
@@ -15,8 +28,35 @@ public class AppConfig {
     }
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-        ChatClient dashScopeChatClient = chatClientBuilder
+    public List<NamedClientMcpTransport> webFluxClientTransports(McpSseClientProperties sseProperties,
+                                                                 ObjectProvider<WebClient.Builder> webClientBuilderProvider,
+                                                                 ObjectProvider<ObjectMapper> objectMapperProvider) {
+        WebClient.Builder webClientBuilder = webClientBuilderProvider.getIfAvailable(WebClient::builder);
+        ObjectMapper objectMapper = objectMapperProvider.getIfAvailable(ObjectMapper::new);
+
+        List<NamedClientMcpTransport> namedClientMcpTransports = new ArrayList<>();
+        for (Map.Entry<String, McpSseClientProperties.SseParameters> entry : sseProperties.getConnections().entrySet()) {
+            String sseName = entry.getKey();
+            McpSseClientProperties.SseParameters sseParameters = entry.getValue();
+            WebClient.Builder webClientBuilderClone = webClientBuilder
+                    .clone()
+                    .baseUrl(sseParameters.url())
+                    .defaultHeaders(headers -> headers.add("Content-Type", "application/json"));
+            String sseEndpoint = sseParameters.sseEndpoint() != null ? sseParameters.sseEndpoint() : "/sse";
+            WebFluxSseClientTransport webFluxSseClientTransport = WebFluxSseClientTransport
+                    .builder(webClientBuilderClone)
+                    .sseEndpoint(sseEndpoint)
+                    .objectMapper(objectMapper)
+                    .build();
+            namedClientMcpTransports.add(new NamedClientMcpTransport(sseName, webFluxSseClientTransport));
+        }
+
+        return namedClientMcpTransports;
+    }
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ToolCallbackProvider toolCallbackProvider) {
+        ChatClient chatClient = chatClientBuilder
                 .defaultSystem("你是一个博学的智能聊天助手，请根据用户提问回答！")
                 // TODO
                 // 实现 Chat Memory 的 Advisor
@@ -36,8 +76,9 @@ public class AppConfig {
                                 .withEnableThinking(true)
                                 .build()
                 )
+                .defaultToolCallbacks(toolCallbackProvider)
                 .build();
 
-        return dashScopeChatClient;
+        return chatClient;
     }
 }
